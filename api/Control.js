307d5e13a9bd8b3822712f8dee5565a084312f6a
lollp18 <lorenzo123696@gmail.com>
2024-07-20 10:15:54 +0200

    ü

--- api/Control.js
+++ api/Control.js
@@ -31,6 +31,12 @@ class Controller extends Model {
   }
 
   InitUse() {
+    this.App.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "React app URL")
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+      res.header("Access-Control-Allow-Headers", "Content-Type")
+      next()
+    })
     this.App.use(cors(this.CorsOptions))
     this.App.use(express.json())
     this.App.use(compression())
